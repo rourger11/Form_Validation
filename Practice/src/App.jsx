e9689ba0a53ea90fc2b698badb819742cb506bea
rouger11 <panwardeepak212@gmail.com>
2025-03-04 12:30:06 +0530

    edit in validation

--- Practice/src/App.jsx
+++ Practice/src/App.jsx
@@ -1,76 +1,125 @@
-
-import { useEffect, useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
-  const[inputValue,setInputValue]=useState({
-    name:"",
-    email:"",
-    number:"",
+  const [inputValue, setInputValue] = useState({
+    name: "",
+    email: "",
+    number: "",
   });
 
-  const [nameError,setNameError]=useState("")
-  const [emailError,setemailError]=useState("")
-  const [numberError,setNumberError]=useState("")
-
+    const [error,setError]=useState({
+      name:false,
+      email:false,
+      number:false
+    })
+ 
 
   let emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-  const handleInput=(e)=>{
-    
-    const {name,value}=e.target;
+
+  const handleInput = (e) => {
+    const { name, value } = e.target;
     setInputValue({
       ...inputValue,
-      [name]:value
+      [name]: value,
     });
-    setNameError("")
-    setemailError('')
-    setNumberError('')
+    setError({
+      name:false,
+      email:false,
+      number:false
+    })
+   
+  };
 
-  }
+  const handleSubmit = (e) => {
 
+    let isValid=true
+    e.preventDefault();
+    Validation();
+    if(isValid){
+      console.log(inputValue)
+    }
+  };
 
-  const handleSubmit=(e)=>{
-  e.preventDefault();
-  Validation()
+  const Validation = () => {
+    if (inputValue.name === "") {
+      setError((prev)=>({
+        ...prev, name:true
+      }));
+      isValid=false
+    }
+    if (!emailRegex.test(inputValue.email)) {
+      setError((prev)=>({
+        ...prev, email:true
+      }));
+      isValid=false
 
-  if(!nameError && !emailError && !numberError){
-    console.log("done");
-  }
+    }
+    if (inputValue.number.length !== 10) {
+      setError((prev)=>({
+        ...prev, number:true
+      }));
+      isValid=false
 
- }
-
- const Validation=()=>{
-
-  if (inputValue.name===""){
-    setNameError("enter name please")
-  }
-  if (!emailRegex.test(inputValue.email)){
-    setemailError("enter a valid email please")
-  }
-  if (inputValue.number.length!==10){
-    setNumberError("enter a valid phone number please")
-  }
-
-}
-
+    }
+  };
 
   return (
     <>
-     <section>
-      <form  onSubmit={handleSubmit} style={{display:'flex', flexFlow:'column', width:'30%',justifySelf: 'center', marginTop:'10%', justifyContent:'space-between',rowGap:'10px'} }>
-        <input type='text'  onChange={(e)=>{handleInput(e)}}name='name' placeholder='enter your name'/>
-        <span> {nameError}</span>
-        <input type='text' onChange={(e)=>{handleInput(e)}} name='email' placeholder='enter your email'/>
-        <span> {emailError}</span>
-        <input type='text' onChange={(e)=>{handleInput(e)}} name='number' placeholder='enter your phone number'/>
-        <span> {numberError}</span>
+      <section>
+        <form
+          onSubmit={handleSubmit}
+          style={{
+            display: "flex",
+            flexFlow: "column",
+            width: "30%",
+            justifySelf: "center",
+            marginTop: "10%",
+            justifyContent: "space-between",
+            rowGap: "10px",
+          }}
+        >
+          <input
+            type="text"
+            onChange={(e) => {
+              handleInput(e);
+            }}
+            name="name"
+            placeholder="enter your name"
+          />
+          <span style={{ color: "red" }}>
+            
+            {error.name && "enter a valid name"}
+          </span>
+          <input
+            type="text"
+            onChange={(e) => {
+              handleInput(e);
+            }}
+            name="email"
+            placeholder="enter your email"
+          />
+          <span style={{ color: "red" }}>
+            
+            {error.email && "enter a valid email"}
+          </span>
+          <input
+            type="text"
+            onChange={(e) => {
+              handleInput(e);
+            }}
+            name="number"
+            placeholder="enter your phone number"
+          />
+          <span style={{ color: "red" }}>
+            {error.number && "enter a valid number"}
+          </span>
 
-        <button type='submit'>Submit</button>
-      </form> 
-     </section>
+          <button type="submit">Submit</button>
+        </form>
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
